6bf047c934fcea5d6fff999e2d520755c89671d2
btolsma <btol201197@gmail.com>
2025-12-03 23:51:21 +0300

    change locations

--- src/stores/llm/providers/CohereProvider.py
+++ src/stores/llm/providers/CohereProvider.py
@@ -1,7 +1,7 @@
-from ...LLMInterface import LLMInterface
+from ..LLMInterface import LLMInterface
 from openai import OpenAI
 import logging
-from ...LLMEnums import CohereEnums, DocumentType
+from ..LLMEnums import CohereEnums, DocumentType
 import cohere
 
 
